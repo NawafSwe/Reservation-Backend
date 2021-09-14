aa544a386344aefb1379561fe9f5a8a25dec90fb
NawafSwe <nawaf.jr9@hotmail.com>
2021-09-14 14:44:11 +0300

    feat: injecting table routes into the server, to stand out with restaurant model

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import App from './app';
 import indexRoutes from './routes/index.route';
 import restaurantsRoutes from './routes/restaurants.route';
-const app: App = new App([indexRoutes,restaurantsRoutes]);
+import tablesRoutes from './routes/table.route';
+const app: App = new App([indexRoutes, restaurantsRoutes, tablesRoutes]);
 // start listening
 app.listen();
 
---
+++ src/routes/table.route.ts
@@ -0,0 +1,56 @@
+import { Router, Request, Response } from 'express';
+import Routes from '@interfaces/routes.interface';
+import * as tableControllers from '../controllers/tables.controller';
+
+const router: Router = Router();
+router.get(`/`, async (req: Request, res: Response) => {
+    try {
+        const response = await tableControllers.getAllTables();
+        res.status(200).json(response);
+    } catch (error) {
+        console.error(`error occurred at route, ${req.path}, error: ${error}`);
+    }
+});
+
+router.post('/', async (req: Request, res: Response) => {
+    try {
+        const restaurantID = req.body.restaurantId;
+        delete req.body.restaurantId;
+        const response = await tableControllers.createTable(restaurantID, req.body);
+        res.status(201).json(response);
+
+    } catch (error) {
+        console.error(`error occurred at route, ${req.path}, error`);
+    }
+});
+
+
+router.get('/:id', async (req: Request, res: Response) => {
+    try {
+        const response = await tableControllers.getTableById(req.params.id);
+        res.status(200).json(response);
+    } catch (error) {
+        console.error(`error occurred at route, ${req.path}, error`);
+    }
+});
+
+router.put('/:id', async (req: Request, res: Response) => {
+    try {
+        const response = await tableControllers.updateTableById(req.params.id, req.body);
+        res.status(200).json(response);
+    } catch (error) {
+        console.error(`error occurred at route, ${req.path}, error`);
+    }
+});
+
+router.delete('/:id', async (req: Request, res: Response) => {
+    try {
+        const response = await tableControllers.deleteTableById(req.params.id);
+        res.status(200).json(response);
+
+    } catch (error) {
+        console.error(`error occurred at route, ${req.path}, error`);
+    }
+})
+export default { router: router, path: '/tables' } as Routes;
+
