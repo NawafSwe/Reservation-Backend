8fc871a77c13d0f3cc49e165d609dd9f14c62c5c
NawafSwe <nawaf.jr9@hotmail.com>
2021-09-14 14:41:23 +0300

    feat:implement table controller/services

--- src/controllers/tables.controller.ts
+++ src/controllers/tables.controller.ts
@@ -1,11 +1,47 @@
-export const getAllTables = async() => {};
+import Table from '../models/table.model';
+import * as tableServices from '../services/table.service';
+export const getAllTables = async () => {
+    try {
+        return await tableServices.getTables();
 
-export const createTable = async () => { }; 
+    } catch (error) {
+        console.log(`error occurred at tableControllers, at getAllTables, error: ${error}`);
+    }
+};
 
-export const getTableById = async () =>{ };
+export const createTable = async (id: string, body: Table) => {
+    try {
+        return await tableServices.createTable(id, body);
 
-export const updateTableById = async () => { };
+    } catch (error) {
+        console.log(`error occurred at tableControllers, at createTable, error: ${error}`);
+    }
+};
+
+export const getTableById = async (id: string) => {
+    try {
+        return await tableServices.getTableById(id);
+
+    } catch (error) {
+        console.log(`error occurred at tableControllers, at getTableById, error: ${error}`);
+    }
+};
+
+export const updateTableById = async (id: string, body: Table) => {
+    try {
+        return await tableServices.updateTableById(id, body);
+    } catch (error) {
+        console.log(`error occurred at table controllers, at updateTableById, error: ${error}`);
+    }
+};
 
 
 // TODO: if table have future reservations do not delete it 
-export const deleteTableById =async () => { };
+export const deleteTableById = async (id:string) => {
+    try {
+        return await tableServices.deleteTableById(id);
+
+    } catch (error) {
+        console.log(`error occurred at table controllers, at deleteTableById,error:${error}`);
+    }
+ };
--- src/services/table.service.ts
+++ src/services/table.service.ts
@@ -1,9 +1,59 @@
-export const getTables = async () => { };
+import Table from '../models/table.model';
+import Restaurant from '../models/restaurant.model';
+import * as restaurantServices from './restaurant.service';
+import { DeleteResult, getRepository, UpdateResult } from 'typeorm';
+export const getTables = async (): Promise<Array<Table> | never> => {
+    try {
+        return await getRepository(Table).find({ relations: ['restaurant', 'reservations'] });
+    } catch (error) {
+        console.error(`error occurred at tables services, at getTables, error: ${error}`);
+    }
+};
 
-export const createTable = async () => { };
-export const getTableById = async () => { };
+/**
+ * 
+ * @param {string} id of restaurant
+ * @param {Table} table data
+ */
+export const createTable = async (id: string, table: Table): Promise<Table | never> => {
+    try {
+        const findRestaurant: Restaurant = await restaurantServices.getRestaurantById(id);
+        const tableRepository = await getRepository(Table);
+        const insertTableResponse = await tableRepository.create({ ...table, restaurant: findRestaurant });
+        await tableRepository.save(insertTableResponse);
+        return insertTableResponse;
+
+    } catch (error) {
+        console.error(`error occurred at tables services, at createTable, error: ${error}`);
+    }
+};
 
 
-export const deleteTableById = async () => { };
+export const getTableById = async (id: string): Promise<Table | never> => {
+    try {
+        return await getRepository(Table).findOne(id, { relations: ['restaurant', 'reservations'] });
+    } catch (error) {
+        console.error(`error occurred at tables services, at getTableById, error: ${error}`);
+    }
 
-export const updateTableById = async () => { };
+};
+
+
+export const deleteTableById = async (id: string): Promise<DeleteResult | never> => {
+    try {
+        return await getRepository(Table).delete(id);
+
+
+    } catch (error) {
+        console.error(`error occurred at tables services, at deleteTableById, error: ${error}`);
+    }
+};
+
+export const updateTableById = async (id: string, table: Table): Promise<UpdateResult | never> => {
+    try {
+        return await getRepository(Table).update(id, table);
+    } catch (error) {
+        console.error(`error occurred at tables services, at updateTableById, error: ${error}`);
+    }
+
+};
