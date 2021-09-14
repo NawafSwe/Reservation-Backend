1ff6602e472398e6eed70b589030c48bf51be05a
NawafSwe <nawaf.jr9@hotmail.com>
2021-09-14 14:33:46 +0300

    feat:init table controllers/services with curd functions

---
+++ src/controllers/tables.controller.ts
@@ -0,0 +1,11 @@
+export const getAllTables = async() => {};
+
+export const createTable = async () => { }; 
+
+export const getTableById = async () =>{ };
+
+export const updateTableById = async () => { };
+
+
+// TODO: if table have future reservations do not delete it 
+export const deleteTableById =async () => { };
---
+++ src/services/table.service.ts
@@ -0,0 +1,9 @@
+export const getTables = async () => { };
+
+export const createTable = async () => { };
+export const getTableById = async () => { };
+
+
+export const deleteTableById = async () => { };
+
+export const updateTableById = async () => { };
