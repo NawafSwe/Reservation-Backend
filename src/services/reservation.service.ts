a1e6ff03279cad9f35c004b980f04142cb0d24d2
NawafSwe <nawaf.jr9@hotmail.com>
2021-09-14 14:54:16 +0300

    feat: implement createReservation and pickTableForReservation functions as a service

--- src/services/reservation.service.ts
+++ src/services/reservation.service.ts
@@ -8,24 +8,29 @@ import dayjs from "dayjs";
  * @param skip where from entities should be taken.
  * @param take max number of entities should be taken.
  * @returns 
- * @description finding all reservations 
+ * @description finding all reservations based on given period or fetch for today 
  */
 
-export const getAllReservations = async (skip?: number, take?: number) => {
+export const getAllReservations = async (skip?: number, take?: number, period?: Date) => {
     try {
 
         const reservationRepository = await getRepository(Reservation);
         if (skip && take) {
             return await reservationRepository.findAndCount({
                 skip, take, relations: ['table'], where: {
-                    // fetching reservation for today
-                    createdAtString: dayjs(new Date('2021-08-13T13:38:17.333Z')).format('DD/MM/YYYY')
+                    createdAtString: dayjs(period != null ? period : new Date()).format('DD/MM/YYYY')
 
                 }
             });
         } else {
 
-            return await reservationRepository.find({ relations: ['table'], });
+            return await reservationRepository.find({
+                relations: ['table'],
+                where: {
+                    createdAtString: dayjs(period != null ? period : new Date()).format('DD/MM/YYYY')
+
+                }
+            });
         }
 
     } catch (error) {
@@ -34,9 +39,19 @@ export const getAllReservations = async (skip?: number, take?: number) => {
 
 };
 
-export const getAllTodayReservation = async () => { };
-
-export const createReservation = async () => { };
+export const createReservation = async (restaurant: Restaurant, reservation: Reservation) => {
+    try {
+        const findTable: Table = await pickTableForReservation(restaurant.tables, reservation);
+        if (findTable) {
+            const reservationRepository = await getRepository(Reservation);
+            const reservationCreationResult = await reservationRepository.create({ ...reservation, table: findTable, createdAtString: dayjs(new Date()).format('DD/MM/YYYY') });
+            return await reservationRepository.save(reservationCreationResult);
+        }
+        else null;
+    } catch (error) {
+        console.error(`error occurred at reservation services, at createReservation, error: ${error}`);
+    }
+};
 
 export const getReservationById = async () => { };
 
@@ -46,4 +61,12 @@ export const deleteReservationById = async () => { };
 
 export const findConflictReservations = async () => { };
 
-const pickTableForReservation = () => { };
+const pickTableForReservation = (tables: Array<Table>, reservationDetails: Reservation): Table | null => {
+    let selectedTable: Table;
+
+    for (const table of tables) {
+        // either table full or one seat empty to maximize profit
+        if (table.capacity === reservationDetails.numberOfClients + 1 || table.capacity === reservationDetails.numberOfClients) selectedTable = table;
+    }
+    return selectedTable;
+};
