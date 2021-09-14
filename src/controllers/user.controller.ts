7da02383278ff2151d6390bc031cbb656a307773
NawafSwe <nawaf.jr9@hotmail.com>
2021-09-14 16:49:03 +0300

    feat:implement user controllers

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,47 @@
-export const getAllUsers = async () => { };
+import * as userServices from '../services/users.service';
+import UserDto from '../dtos/user.dto';
 
-export const getUserById = async () => { };
-export const createUser = async () => { };
+export const getAllUsers = async () => {
+    try {
+        const response = await userServices.getAllUsers();
+        return response;
+    } catch (error) {
+        console.error(`error occurred, at users controllers at getAllUsers, error: ${error}`);
+    }
+};
 
-export const updateUserById = async () => { };
-export const deleteUserById = async () => { };
+export const getUserById = async (id: string) => {
+    try {
+        const response = await userServices.getUserById(id);
+        return response;
+    } catch (error) {
+        console.error(`error occurred, at users controllers at getUserById, error: ${error}`);
+    }
+};
+export const createUser = async (user: UserDto) => {
+    try {
+        // check if user exist or no 
+        const response = await userServices.createUser(user);
+        return response;
+    } catch (error) {
+        console.error(`error occurred, at users controllers at createUser, error : ${error}`);
+    }
+};
+
+export const updateUserById = async (id: string, user: UserDto) => {
+    try {
+        // check if there is no conflicts with emp number 
+        const response = await userServices.updateUserById(id, user);
+        return response;
+    } catch (error) {
+        console.error(`error occurred, at user controllers at updateUserById, error: ${error}`);
+    }
+};
+export const deleteUserById = async (id: string) => {
+    try {
+        const response = await userServices.deleteUserById(id);
+        return response;
+    } catch (error) {
+        console.error(`error occurred, at user controllers at deleteUserById, error: ${error}`);
+    }
+};
