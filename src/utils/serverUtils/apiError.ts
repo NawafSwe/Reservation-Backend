90f7bda94f9d3ba97cc5597becf5433b53504ea8
NawafSwe <nawaf.jr9@hotmail.com>
2021-09-14 18:34:31 +0300

    feat: create APIError for better handling

---
+++ src/utils/serverUtils/apiError.ts
@@ -0,0 +1,32 @@
+import { httpStatus } from './httpStatus';
+/**
+ * @module APIError
+ * @requires httpStatus
+ * @description module holds centralized server error.
+ */
+
+
+export default class APIError extends Error {
+    public name;
+    public statusCode;
+    public isOperational;
+    public description;
+
+    /**
+     * @param {Object<httpStatus>} http
+     * @param {boolean} isOperational type of the error operational versus programmer
+     * @param {string} description of the error
+     */
+    // custom error we can delete name or isOperational to make it more simple
+    constructor(http: any, description: string, isOperational?: boolean) {
+        super(description);
+        // using object.create for better performance ;;
+        Object.setPrototypeOf(this, new.target.prototype);
+        this.name = http.message;
+        this.statusCode = http.code;
+        this.isOperational = isOperational == null ? false : isOperational;
+        this.description = description;
+        // capture stack of the error 'where it occurred';
+        Error.captureStackTrace(this);
+    }
+}
