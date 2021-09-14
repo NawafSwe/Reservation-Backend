7a64e792b995d079d0b5e98972ff62403f4f0ecf
NawafSwe <nawaf.jr9@hotmail.com>
2021-09-14 15:02:23 +0300

    feat: implement reservation services 🚀🚀

--- src/services/reservation.service.ts
+++ src/services/reservation.service.ts
@@ -1,6 +1,6 @@
 
 import Reservation from "../models/reservation.model";
-import { getRepository } from 'typeorm';
+import { DeleteResult, getRepository, UpdateResult } from 'typeorm';
 import Table from '../models/table.model';
 import Restaurant from "../models/restaurant.model";
 import dayjs from "dayjs";
@@ -11,7 +11,8 @@ import dayjs from "dayjs";
  * @description finding all reservations based on given period or fetch for today 
  */
 
-export const getAllReservations = async (skip?: number, take?: number, period?: Date) => {
+// TODO: deleted status false 
+export const getAllReservations = async (skip?: number, take?: number, period?: Date): Promise<any | never> => {
     try {
 
         const reservationRepository = await getRepository(Reservation);
@@ -39,7 +40,7 @@ export const getAllReservations = async (skip?: number, take?: number, period?: 
 
 };
 
-export const createReservation = async (restaurant: Restaurant, reservation: Reservation) => {
+export const createReservation = async (restaurant: Restaurant, reservation: Reservation): Promise<Reservation | never> => {
     try {
         const findTable: Table = await pickTableForReservation(restaurant.tables, reservation);
         if (findTable) {
@@ -53,13 +54,60 @@ export const createReservation = async (restaurant: Restaurant, reservation: Res
     }
 };
 
-export const getReservationById = async () => { };
+export const getReservationById = async (id: string): Promise<Reservation | never> => {
+    try {
+        const reservationRepository = await getRepository(Reservation);
+        return await reservationRepository.findOne(id, { relations: ['table'] });
+    } catch (error) {
+        console.error(`error occurred at reservation services, getReservationById, error: ${error}`);
+    }
 
-export const updateReservationById = async () => { };
+};
 
-export const deleteReservationById = async () => { };
+export const updateReservationById = async (id: string, body: Reservation): Promise<UpdateResult | never> => {
+    try {
+        const reservationRepository = await getRepository(Reservation);
+        return await reservationRepository.update(id, body);
 
-export const findConflictReservations = async () => { };
+    } catch (error) {
+        console.error(`error occurred at reservation services, updateReservationById, error: ${error}`);
+    }
+};
+
+// TODO: soft delete, to keep record in 
+export const deleteReservationById = async (id: string): Promise<DeleteResult | never> => {
+    try {
+        const reservationRepository = await getRepository(Reservation);
+        return await reservationRepository.delete(id);
+
+    } catch (error) {
+        console.error(`error occurred at reservation services, deleteReservationById, error: ${error}`);
+    }
+};
+
+export const findConflictReservations = async (tables: Array<Table>, reservationDetails: Reservation): Promise<Array<Reservation | never>> => {
+    // ===> query where starting == given starting, ending === giving ending;
+    // ===> starting date != existing starting date of reservation
+    // ===> ending date of existing reservation can equal starting date of new reservation 
+    // ===> ending date cannot be equal to existing ending date of reservation 
+    try {
+        let selectedTable: Table = pickTableForReservation(tables, reservationDetails);
+        if (selectedTable) {
+            return await getRepository(Reservation).find(
+                {
+                    where: [
+                        // counting all corresponding times
+                        { table: selectedTable, staringHoursDate: reservationDetails.staringHoursDate, endingHoursDate: reservationDetails.endingHoursDate },
+                        { table: selectedTable, staringHoursDate: reservationDetails.staringHoursDate },
+                        { table: selectedTable, endingHoursDate: reservationDetails.endingHoursDate },
+                    ]
+                });
+        }
+        else return [];
+    } catch (error) {
+        console.error(`error occurred at tables services, at findConflictReservations, error: ${error}`);
+    }
+};
 
 const pickTableForReservation = (tables: Array<Table>, reservationDetails: Reservation): Table | null => {
     let selectedTable: Table;
