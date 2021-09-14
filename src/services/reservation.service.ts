44237d1668201252501df1a4002249d126bc404c
NawafSwe <nawaf.jr9@hotmail.com>
2021-09-14 14:49:34 +0300

    feat: implement getAllReservations for specific table for any time

--- src/services/reservation.service.ts
+++ src/services/reservation.service.ts
@@ -1,17 +1,49 @@
 
+import Reservation from "../models/reservation.model";
+import { getRepository } from 'typeorm';
+import Table from '../models/table.model';
+import Restaurant from "../models/restaurant.model";
+import dayjs from "dayjs";
+/**
+ * @param skip where from entities should be taken.
+ * @param take max number of entities should be taken.
+ * @returns 
+ * @description finding all reservations 
+ */
 
-export const getAllReservations = async()=>{};
+export const getAllReservations = async (skip?: number, take?: number) => {
+    try {
 
-export const getAllTodayReservation = async()=>{};
+        const reservationRepository = await getRepository(Reservation);
+        if (skip && take) {
+            return await reservationRepository.findAndCount({
+                skip, take, relations: ['table'], where: {
+                    // fetching reservation for today
+                    createdAtString: dayjs(new Date('2021-08-13T13:38:17.333Z')).format('DD/MM/YYYY')
 
-export const createReservation = async()=>{};
+                }
+            });
+        } else {
 
-export const getReservationById = async()=>{};
+            return await reservationRepository.find({ relations: ['table'], });
+        }
 
-export const updateReservationById = async()=>{};
+    } catch (error) {
+        console.log(`error occurred at reservations service, at getAllReservations, error: ${error}`);
+    }
 
-export const deleteReservationById = async()=>{};
+};
 
-export const findConflictReservations = async()=>{};
+export const getAllTodayReservation = async () => { };
 
-const pickTableForReservation = ()=>{};
+export const createReservation = async () => { };
+
+export const getReservationById = async () => { };
+
+export const updateReservationById = async () => { };
+
+export const deleteReservationById = async () => { };
+
+export const findConflictReservations = async () => { };
+
+const pickTableForReservation = () => { };
