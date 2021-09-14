049b7cbebdda15e9071c6fceb1f1b95e108376a2
NawafSwe <nawaf.jr9@hotmail.com>
2021-09-14 14:47:20 +0300

    feat: prepare reservation controllers/services

---
+++ src/controllers/reservation.controller.ts
@@ -0,0 +1,6 @@
+
+export const getAllReservation = async () => { };
+export const reserveTable = async () => { };
+export const getReservationById = async () => { };
+export const updateReservationById = async () => { };
+export const deleteReservationById = async () => { };
---
+++ src/services/reservation.service.ts
@@ -0,0 +1,17 @@
+
+
+export const getAllReservations = async()=>{};
+
+export const getAllTodayReservation = async()=>{};
+
+export const createReservation = async()=>{};
+
+export const getReservationById = async()=>{};
+
+export const updateReservationById = async()=>{};
+
+export const deleteReservationById = async()=>{};
+
+export const findConflictReservations = async()=>{};
+
+const pickTableForReservation = ()=>{};
