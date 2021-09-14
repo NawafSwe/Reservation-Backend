2a2ffc23c97aeed7bdcd8ac07fe3c462a15e3704
NawafSwe <nawaf.jr9@hotmail.com>
2021-09-14 14:04:21 +0300

    feat:init restaurant services with basic curd op

---
+++ src/services/restaurant.service.ts
@@ -0,0 +1,13 @@
+import { getRepository } from 'typeorm';
+import Restaurant from '../models/restaurant.model';
+
+
+export const getRestaurants = async () => {};
+
+export const createRestaurant = async()=>{};
+
+export const getRestaurantById = async () => {};
+
+export const deleteRestaurantById = async ()=>{};
+
+export const updateRestaurantById = async () =>{};
