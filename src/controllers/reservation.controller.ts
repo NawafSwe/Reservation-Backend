f8f21f9595b220b17de1555bf2beb995608daaab
NawafSwe <nawaf.jr9@hotmail.com>
2021-09-14 15:06:24 +0300

    feat: implement reservation controllers on top of reservation services

--- src/controllers/reservation.controller.ts
+++ src/controllers/reservation.controller.ts
@@ -1,6 +1,34 @@
-
-export const getAllReservation = async () => { };
+import Reservation from '../models/reservation.model';
+import * as reservationServices from '../services/reservation.service';
+export const getAllReservation = async () => {
+    try {
+        return await reservationServices.getAllReservations();
+    } catch (error) {
+        console.error(`error occurred at reservations controllers, at getAllReservation, error: ${error}`);
+    }
+};
 export const reserveTable = async () => { };
-export const getReservationById = async () => { };
-export const updateReservationById = async () => { };
-export const deleteReservationById = async () => { };
+
+export const getReservationById = async (id: string) => {
+    try {
+        return await reservationServices.getReservationById(id);
+    } catch (error) {
+        console.error(`error occurred at reservation controllers, at getReservationById, error: ${error}`);
+    }
+};
+
+export const updateReservationById = async (id: string, body: Reservation) => {
+    try {
+        return await reservationServices.updateReservationById(id, body);
+    } catch (error) {
+        console.error(`error occurred at reservation controllers, at getReservationById, error: ${error}`);
+    }
+
+};
+export const deleteReservationById = async (id: string) => {
+    try {
+        return await reservationServices.deleteReservationById(id);
+    } catch (error) {
+        console.error(`error occurred at reservation controllers, at deleteReservationById, error: ${error}`)
+    }
+};
