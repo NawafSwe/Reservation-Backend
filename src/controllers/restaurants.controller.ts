ed3f41149ef655a482103d31de7192fb23536151
NawafSwe <nawaf.jr9@hotmail.com>
2021-09-14 14:29:45 +0300

    feat:restaurant controllers implemented on top of services

--- src/controllers/restaurants.controller.ts
+++ src/controllers/restaurants.controller.ts
@@ -1,12 +1,52 @@
-import Restaurant from "@/models/restaurant.model";
+import Restaurant from "../models/restaurant.model";
+import * as restaurantServices from '../services/restaurant.service';
+import dayjs from 'dayjs';
+export const getAllRestaurants = async () => {
+    try {
+        return await restaurantServices.getRestaurants();
+    } catch (error) {
+        console.error(`error occurred at restaurant controllers, at getAllRestaurants, error: ${error}`);
+    }
+ };
+export const createRestaurant = async (restaurant: Restaurant) => {
+    try {
+        // convert time into hours format 
+        restaurant.startingWorkingHoursString = dayjs(restaurant.startingWorkingHoursDate).format('HH:mm');
+        restaurant.endingWorkingHoursString = dayjs(restaurant.endingWorkingHoursDate).format('HH:mm');
+        return await restaurantServices.createRestaurant(restaurant);
+    } catch (error) {
+        console.error(`error occurred at restaurant controllers, at createRestaurant, error: ${error}`);
+    }
+ };
 
-export const getAllRestaurants = async () => { };
+export const getRestaurantById = async (id: string) => { 
+    try {
+        return await restaurantServices.getRestaurantById(id);
+    } catch (error) {
+        console.error(`error occurred at restaurant controllers, at getRestaurantById, error: ${error}`);
+    }
+};
 
-export const createRestaurant = async (restaurant: Restaurant) => { };
+export const deleteRestaurantById = async (id: string) => { 
+    try {
+        return await restaurantServices.deleteRestaurantById(id);
+    } catch (error) {
+        console.error(`error occurred at restaurant controllers, at deleteRestaurantById, error: ${error}`);
+    }
+};
 
-export const getRestaurantById = async (id: string) => { };
 
-export const deleteRestaurantById = async (id: string) => { };
-
-
-export const updateRestaurantById = async (id: string,body: Restaurant) => { };
+export const updateRestaurantById = async (id: string,body: Restaurant) => { 
+    try {
+        // formatting date again if there is changes 
+        if (body.startingWorkingHoursDate) {
+            body.startingWorkingHoursString = dayjs(body.startingWorkingHoursDate).format('HH:mm');
+        }
+        if (body.endingWorkingHoursDate) {
+            body.endingWorkingHoursString = dayjs(body.endingWorkingHoursDate).format('HH:mm');
+        }
+        return await restaurantServices.updateRestaurantById(id, body);
+    } catch (error) {
+        console.error(`error occurred at restaurant controllers, at updateRestaurantById, error: ${error}`);
+    }
+};
