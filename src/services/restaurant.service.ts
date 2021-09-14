0bc9685bed7c9b12a6cbe47f8ca923744d9e649a
NawafSwe <nawaf.jr9@hotmail.com>
2021-09-14 14:20:02 +0300

    feat:implmenting all restaurant services functions

--- src/services/restaurant.service.ts
+++ src/services/restaurant.service.ts
@@ -1,13 +1,46 @@
-import { getRepository } from 'typeorm';
+import { DeleteResult, getRepository } from 'typeorm';
 import Restaurant from '../models/restaurant.model';
 
 
-export const getRestaurants = async () => {};
+export const getRestaurants = async (): Promise<Array<Restaurant> | never> => {
+    try {
+        const restaurants: Array<Restaurant> = await getRepository(Restaurant).find({
+            relations: ['tables']
+        });
+        return restaurants;
+    } catch (error) {
+        console.error(`error occurred, at restaurant services at getRestaurants function, error: ${error}`);
+    }
+};
 
-export const createRestaurant = async()=>{};
+export const createRestaurant = async (restaurant: Restaurant): Promise<Restaurant | never> => {
+    try {
+        const repository = await getRepository(Restaurant);
+        const response = await repository.create(restaurant);
+        return await repository.save(response);
+    } catch (error) {
+        console.error(`error occurred, at restaurant services at createRestaurant function, error: ${error}`);
+    }
+};
 
-export const getRestaurantById = async () => {};
 
-export const deleteRestaurantById = async ()=>{};
 
-export const updateRestaurantById = async () =>{};
+export const getRestaurantById = async (id: string): Promise<Restaurant | never> => {
+    try {
+        const response: Restaurant = await getRepository(Restaurant).findOne(id, { relations: ['tables'] });
+        return response;
+    } catch (error) {
+        console.error(`error occurred, at restaurant services at getRestaurantById function, error: ${error}`);
+    }
+};
+
+export const deleteRestaurantById = async (id: string): Promise<DeleteResult | never> => {
+    try {
+        const response = await getRepository(Restaurant).delete(id);
+        return response;
+    } catch (error) {
+        console.error(`error occurred, at restaurant services at deleteRestaurant function, error: ${error}`);
+    }
+};
+
+export const updateRestaurantById = async () => { };
