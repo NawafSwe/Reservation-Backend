ba8f86e8b9aee6ac1ac11942833a7546dafe6391
NawafSwe <nawaf.jr9@hotmail.com>
2021-09-14 14:08:27 +0300

    feat: init restaurant controllers with basic curd functions to be used in routing

---
+++ src/controllers/restaurants.controller.ts
@@ -0,0 +1,10 @@
+export const getAllRestaurants = async () => { };
+
+export const createRestaurant = async () => { };
+
+export const getRestaurantById = async () => { };
+
+export const deleteRestaurantById = async () => { };
+
+
+export const updateRestaurantById = async () => { };
